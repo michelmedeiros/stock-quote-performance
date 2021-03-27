dae3bd13ecccf2aea715114c9bb808f5c9b8633c
Michel Medeiros <michel.tds@gmail.com>
2021-03-27 08:32:36 -0300

    Add new tests

--- src/test/scala/simulations/reactive/remote/RuntimeParametersRemoteStatusInvestMVC.scala
+++ src/test/scala/simulations/reactive/remote/RuntimeParametersRemoteStatusInvestMVC.scala
@@ -5,7 +5,7 @@ import io.gatling.http.Predef._
 
 import scala.concurrent.duration._
 
-class RuntimeParametersRemotelStatusInvestStatisticsMVC extends Simulation {
+class RuntimeParametersRemoteStatusInvestMVC extends Simulation {
 
   private def getProperty(propertyName: String, defaultValue: String) = {
     Option(System.getenv(propertyName))
@@ -13,17 +13,12 @@ class RuntimeParametersRemotelStatusInvestStatisticsMVC extends Simulation {
       .getOrElse(defaultValue)
   }
 
-  def userCount: Int = getProperty("USERS", "2000").toInt
-  def rampDuration: Int = getProperty("RAMP_DURATION", "30").toInt
-  def testDuration: Int = getProperty("DURATION", "120").toInt
-  def userConstantCount: Int = getProperty("USERS", "1").toInt
-  def constantRamp: Int = getProperty("CONSTANT_RAMP_DURATION", "10").toInt
-
+  def userCount: Int = getProperty("USERS", "500").toInt
+  def rampDuration: Int = getProperty("RAMP_DURATION", "10").toInt
+  def testDuration: Int = getProperty("DURATION3", "60").toInt
 
   before {
     println(s"Running test with ${userCount} fixed users")
-    println(s"Ramping users over ${rampDuration} seconds")
-    println(s"Total test duration: ${testDuration} seconds")
   }
 
   val httpConf = http.baseUrl("http://localhost:8080/statusInvest")
@@ -33,12 +28,12 @@ class RuntimeParametersRemotelStatusInvestStatisticsMVC extends Simulation {
   def getSpecificStockTicker() = {
     feed(csvFeeder)
       .exec(http("Get Status Invest stock: ${ticker}")
-        .get("/statistics/${ticker}")
+        .get("/stocks/generate/${ticker}")
         .check(status.is(200)))
       .pause(1 second)
   }
 
-  val scn = scenario("Get Status Invest statistics stock")
+  val scn = scenario("Generate stock")
     .forever() {
       exec(getSpecificStockTicker())
     }
@@ -46,10 +41,10 @@ class RuntimeParametersRemotelStatusInvestStatisticsMVC extends Simulation {
   setUp(
     scn.inject(
       nothingFor(5 seconds),
-      rampUsers(userCount) during (rampDuration seconds),
-      constantUsersPerSec(userConstantCount) during (constantRamp seconds)
+      rampUsers(userCount) during (rampDuration seconds)
     )
   ).protocols(httpConf)
     .maxDuration(testDuration seconds)
+
 }
 
